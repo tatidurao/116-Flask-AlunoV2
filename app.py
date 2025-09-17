ae8772cc6863d95f40fdb290a701d912d577db93
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2025-09-17 14:03:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,18 @@ app = Flask(__name__)
 # rota padrão ou 'URL'
 @app.route("/index")
 def home():
-    nome = "Kaiowa" # escreva seu nome
-    idade = "14" # escreva sua idade
+    nome = "" # escreva seu nome
+    idade = "" # escreva sua idade
     return render_template('index.html' , nome = nome , idade = idade)
 
 # defina a rota para a página do pai
-@app.route("/pai")
-def home1():
-    nome = "Louis" # escreva seu nome
-    idade = "32" # escreva sua idade
-    return render_template('pai.html' , nome = nome , idade = idade)
+
 
 # defina a rota para a página da mãe
-@app.route("/mae")
-def home2():
-    nome = "Preguiça de pensar um nome" # escreva seu nome
-    idade = "31" # escreva sua idade
-    return render_template('mae.html' , nome = nome , idade = idade)
+
 
 # defina a rota para a página do amigo
-@app.route("/amigo")
-def home3():
-    nome = "David" # escreva seu nome
-    idade = "14" # escreva sua idade
-    return render_template('amigo.html' , nome = nome , idade = idade)
+
 
 # adicione outras rotas, se você quiser
 
